d0918ddfe19b38ebd784eb49dfbe173a8cddabf0
Sam Lakerveld <dark@dark.red>
2023-10-30 00:16:09 +0100

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,16 @@
+#![feature(array_chunks)]
+
+mod nacl;
+
 use std::{
+    collections::BTreeMap,
     env,
-    sync::atomic::{AtomicUsize, Ordering},
+    io::Write,
+    ops::Deref,
+    sync::{
+        atomic::{AtomicUsize, Ordering},
+        Arc,
+    },
     thread,
     time::{Duration, Instant},
 };
@@ -10,9 +20,9 @@ use base64::{
     engine::{GeneralPurpose, GeneralPurposeConfig},
     Engine,
 };
-use core_affinity::CoreId;
-use rand_core::OsRng;
-use x25519_dalek_fiat::{PublicKey, StaticSecret};
+// use core_affinity::CoreId;
+// use rand_core::OsRng;
+// use x25519_dalek_fiat::{PublicKey, StaticSecret};
 
 static GENERATED: AtomicUsize = AtomicUsize::new(0);
 
@@ -23,70 +33,363 @@ const KEY_LENGTH: usize = 32;
 const KEY_B64_LENGTH: usize = 44;
 
 fn main() {
+    // sodiumoxide::init().unwrap();
+
     let mut args = env::args().skip(1).collect::<Vec<_>>();
 
     let (tx, rx) = std::sync::mpsc::channel::<[[u8; KEY_LENGTH]; 2]>();
 
     let start = Instant::now();
 
-    thread::spawn(move || loop {
-        core_affinity::set_for_current(CoreId { id: 0 });
-        thread::sleep(Duration::from_secs(60));
-        let cycles = GENERATED.swap(0, Ordering::Relaxed);
-        eprintln!("{cycles} cycles/min");
-    });
+    gpu();
 
-    let checker = thread::spawn(move || {
-        core_affinity::set_for_current(CoreId { id: 0 });
-        let mut pk64 = [0u8; KEY_B64_LENGTH];
-        for [sk, pk] in rx {
-            let _ = BASE64.encode_slice(pk, &mut pk64);
+    // thread::spawn(move || loop {
+    //     core_affinity::set_for_current(CoreId { id: 0 });
+    //     thread::sleep(Duration::from_secs(1));
+    //     let cycles = GENERATED.swap(0, Ordering::Relaxed);
+    //     eprintln!("{cycles} cycles/min");
+    // });
 
-            args.retain(|s| {
-                let retain = !pk64.starts_with(s.as_bytes());
-                if !retain {
-                    eprintln!(
-                        "Found one! {} (after {:.1} minutes)",
-                        String::from_utf8(pk64.to_vec()).unwrap(),
-                        start.elapsed().as_secs_f64() / 60.,
-                    );
-                    println!(
-                        "sk: {} pk: {}",
-                        BASE64.encode(sk),
-                        String::from_utf8(pk64.to_vec()).unwrap()
-                    );
-                }
-                retain
-            });
+    // let checker = thread::spawn(move || {
+    //     core_affinity::set_for_current(CoreId { id: 0 });
+    //     let mut pk64 = [0u8; KEY_B64_LENGTH];
+    //     for [sk, pk] in rx {
+    //         let _ = BASE64.encode_slice(pk, &mut pk64);
 
-            if args.is_empty() {
-                eprintln!("All keys found!");
-                break;
-            }
-        }
-    });
+    //         args.retain(|s| {
+    //             let retain = !pk64.starts_with(s.as_bytes());
+    //             if !retain {
+    //                 eprintln!(
+    //                     "Found one! {} (after {:.1} minutes)",
+    //                     String::from_utf8(pk64.to_vec()).unwrap(),
+    //                     start.elapsed().as_secs_f64() / 60.,
+    //                 );
+    //                 println!(
+    //                     "sk: {} pk: {}",
+    //                     BASE64.encode(sk),
+    //                     String::from_utf8(pk64.to_vec()).unwrap()
+    //                 );
+    //             }
+    //             retain
+    //         });
 
-    let generators = core_affinity::get_core_ids()
-        .unwrap()
-        .into_iter()
-        .map(|core_id| {
-            let tx = tx.clone();
-            thread::spawn(move || {
-                core_affinity::set_for_current(core_id);
+    //         if args.is_empty() {
+    //             eprintln!("All keys found!");
+    //             break;
+    //         }
+    //     }
+    // });
 
-                loop {
-                    let sk = StaticSecret::new(OsRng);
-                    let pk = PublicKey::from(&sk);
+    // let generators = core_affinity::get_core_ids()
+    //     .unwrap()
+    //     .into_iter()
+    //     .map(|core_id| {
+    //         let tx = tx.clone();
+    //         thread::spawn(move || {
+    //             core_affinity::set_for_current(core_id);
 
-                    let _ = tx.send([sk.to_bytes(), pk.to_bytes()]);
+    //             loop {
+    //                 // let sk = StaticSecret::new(OsRng);
+    //                 // let pk = PublicKey::from(&sk);
 
-                    GENERATED.fetch_add(1, Ordering::Relaxed);
-                }
-            });
-        })
+    //                 let (pk, sk) = sodiumoxide::crypto::box_::gen_keypair();
+
+    //                 let _ = tx.send([sk.0, pk.0]);
+
+    //                 GENERATED.fetch_add(1, Ordering::Relaxed);
+    //             }
+    //         });
+    //     })
+    //     .collect::<Vec<_>>();
+
+    // eprintln!("Spawned {} threads...", generators.len());
+
+    // checker.join().unwrap();
+}
+
+use rand::{rngs::OsRng, Fill, RngCore};
+use vulkano::{
+    buffer::{Buffer, BufferCreateInfo, BufferUsage, Subbuffer},
+    command_buffer::{
+        allocator::{StandardCommandBufferAllocator, StandardCommandBufferAllocatorCreateInfo},
+        AutoCommandBufferBuilder, CommandBufferUsage, CopyBufferInfo,
+    },
+    descriptor_set::{
+        allocator::{StandardDescriptorSetAllocator, StandardDescriptorSetAllocatorCreateInfo},
+        layout::{
+            DescriptorSetLayout, DescriptorSetLayoutBinding, DescriptorSetLayoutCreateFlags,
+            DescriptorSetLayoutCreateInfo, DescriptorType,
+        },
+        CopyDescriptorSet, PersistentDescriptorSet, WriteDescriptorSet,
+    },
+    device::{Device, DeviceCreateInfo, QueueCreateInfo, QueueFlags},
+    instance::{Instance, InstanceCreateInfo},
+    memory::{
+        allocator::{
+            AllocationCreateInfo, DeviceLayout, MemoryTypeFilter, StandardMemoryAllocator,
+        },
+        DeviceAlignment,
+    },
+    pipeline::{
+        compute::ComputePipelineCreateInfo,
+        layout::{PipelineDescriptorSetLayoutCreateInfo, PipelineLayoutCreateInfo},
+        ComputePipeline, Pipeline, PipelineBindPoint, PipelineCreateFlags, PipelineLayout,
+        PipelineShaderStageCreateInfo,
+    },
+    shader::{self, EntryPoint, ShaderModule, ShaderModuleCreateInfo},
+    sync::{self, GpuFuture},
+    VulkanLibrary,
+};
+
+use crate::nacl::curve25519_base;
+
+fn gpu() {
+    let library = VulkanLibrary::new().unwrap();
+    let instance = Instance::new(library, InstanceCreateInfo::default()).unwrap();
+
+    let physical_devices = instance
+        .enumerate_physical_devices()
+        .expect("could not enumerate devices")
         .collect::<Vec<_>>();
 
-    eprintln!("Spawned {} threads...", generators.len());
+    eprintln!("Found {} devices", physical_devices.len());
 
-    checker.join().unwrap();
+    let physical_device = physical_devices
+        .into_iter()
+        .next()
+        .expect("no devices available");
+
+    let queue_family_index = physical_device
+        .queue_family_properties()
+        .iter()
+        .position(|queue_family_properties| {
+            queue_family_properties
+                .queue_flags
+                .contains(QueueFlags::COMPUTE)
+        })
+        .expect("couldn't find a graphical queue family") as u32;
+
+    let (device, mut queues) = Device::new(
+        physical_device,
+        DeviceCreateInfo {
+            // here we pass the desired queue family to use by index
+            queue_create_infos: vec![QueueCreateInfo {
+                queue_family_index,
+                ..Default::default()
+            }],
+            ..Default::default()
+        },
+    )
+    .expect("failed to create device");
+
+    let queue = queues.next().unwrap();
+
+    let memory_allocator = Arc::new(StandardMemoryAllocator::new_default(device.clone()));
+
+    const PARALLELISM: u32 = 1024;
+
+    let input_buffer = Buffer::from_iter(
+        memory_allocator.clone(),
+        BufferCreateInfo {
+            usage: BufferUsage::STORAGE_BUFFER | BufferUsage::TRANSFER_SRC,
+            ..Default::default()
+        },
+        AllocationCreateInfo {
+            memory_type_filter: MemoryTypeFilter::HOST_SEQUENTIAL_WRITE,
+            ..Default::default()
+        },
+        (0..(32 * PARALLELISM)).map(|_| 0u32).collect::<Vec<_>>(),
+    )
+    .expect("failed to create buffer");
+
+    let output_buffer = Buffer::from_iter(
+        memory_allocator.clone(),
+        BufferCreateInfo {
+            usage: BufferUsage::STORAGE_BUFFER | BufferUsage::TRANSFER_DST,
+            ..Default::default()
+        },
+        AllocationCreateInfo {
+            memory_type_filter: MemoryTypeFilter::HOST_SEQUENTIAL_WRITE,
+            ..Default::default()
+        },
+        (0..(32 * PARALLELISM)).map(|_| 0u32).collect::<Vec<_>>(),
+    )
+    .expect("failed to create buffer");
+
+    let shader_u8 = include_bytes!("./shader.comp.spv");
+    let mut shader_u32 = Vec::with_capacity((shader_u8.len() + 3) / 4);
+    for chunk in shader_u8.array_chunks() {
+        shader_u32.push(u32::from_le_bytes(*chunk));
+    }
+
+    let shader = ShaderModuleCreateInfo::new(&shader_u32);
+    let shader = unsafe { ShaderModule::new(device.clone(), shader) }.unwrap();
+
+    let descriptor_set_allocator = StandardDescriptorSetAllocator::new(
+        device.clone(),
+        StandardDescriptorSetAllocatorCreateInfo::default(),
+    );
+
+    // let descriptor_set_layout = DescriptorSetLayout::new(
+    //     device.clone(),
+    //     DescriptorSetLayoutCreateInfo {
+    //         bindings: vec![(
+    //             0,
+    //             DescriptorSetLayoutBinding::descriptor_type(DescriptorType::StorageBuffer),
+    //         )]
+    //         .into_iter()
+    //         .collect(),
+    //         ..Default::default()
+    //     },
+    // )
+    // .unwrap();
+
+    // let stage = PipelineShaderStageCreateInfo::new(shader.entry_point("main").unwrap());
+    // let layout = PipelineLayout::new(
+    //     device.clone(),
+    //     PipelineLayoutCreateInfo {
+    //         // set_layouts: vec![descriptor_set_layout.clone()],
+    //         ..Default::default()
+    //     },
+    // )
+    // .unwrap();
+
+    // let compute_pipeline = ComputePipeline::new(
+    //     device.clone(),
+    //     None,
+    //     ComputePipelineCreateInfo::stage_layout(stage, layout),
+    // )
+    // .expect("failed to create compute pipeline");
+
+    // let layout = compute_pipeline.layout().set_layouts().get(0).unwrap();
+
+    // let descriptor_set = PersistentDescriptorSet::new(
+    //     &descriptor_set_allocator,
+    //     layout.clone(),
+    //     [WriteDescriptorSet::buffer(0, input_buffer.clone())],
+    //     [],
+    // )
+    // .unwrap();
+
+    let pipeline = {
+        let stage = PipelineShaderStageCreateInfo::new(shader.entry_point("main").unwrap());
+        let layout = PipelineLayout::new(
+            device.clone(),
+            PipelineDescriptorSetLayoutCreateInfo::from_stages([&stage])
+                .into_pipeline_layout_create_info(device.clone())
+                .unwrap(),
+        )
+        .unwrap();
+
+        ComputePipeline::new(
+            device.clone(),
+            None,
+            ComputePipelineCreateInfo::stage_layout(stage, layout),
+        )
+        .unwrap()
+    };
+
+    let layout = pipeline.layout().set_layouts().get(0).unwrap();
+    let set = PersistentDescriptorSet::new(
+        &descriptor_set_allocator,
+        layout.clone(),
+        [
+            WriteDescriptorSet::buffer(0, input_buffer.clone()),
+            WriteDescriptorSet::buffer(1, output_buffer.clone()),
+        ],
+        [],
+    )
+    .unwrap();
+
+    let command_buffer_allocator = StandardCommandBufferAllocator::new(
+        device.clone(),
+        StandardCommandBufferAllocatorCreateInfo::default(),
+    );
+
+    let mut command_buffer_builder = AutoCommandBufferBuilder::primary(
+        &command_buffer_allocator,
+        queue.queue_family_index(),
+        CommandBufferUsage::MultipleSubmit,
+    )
+    .unwrap();
+
+    let work_group_counts = [PARALLELISM / 32, 1, 1];
+
+    command_buffer_builder
+        .bind_pipeline_compute(pipeline.clone())
+        .unwrap()
+        .bind_descriptor_sets(
+            PipelineBindPoint::Compute,
+            pipeline.layout().clone(),
+            0,
+            set,
+        )
+        .unwrap()
+        .dispatch(work_group_counts)
+        .unwrap();
+
+    let command_buffer = command_buffer_builder.build().unwrap();
+
+    loop {
+        {
+            let mut buf = input_buffer.write().unwrap();
+            buf.iter_mut().for_each(|x| *x = OsRng.next_u32() & 0xff);
+
+            // (0..(buf.len() / 8)).for_each(|idx| {
+            //     buf[idx * 8] &= 0xf8_ff_ff_ff;
+            //     buf[idx * 8 + 7] &= 0x7f_ff_ff_ff;
+            //     buf[idx * 8 + 7] |= 0x40_00_00_00;
+            // });
+        };
+
+        let sk = &input_buffer.read().unwrap()[0..32]
+            .iter()
+            .map(|&x| x as u8)
+            .collect::<Vec<_>>();
+
+        let sk64 = BASE64.encode(&sk);
+
+        println!();
+        println!("sk: {sk64} {sk:02x?}");
+
+        let future = sync::now(device.clone())
+            .then_execute(queue.clone(), command_buffer.clone())
+            .unwrap()
+            .then_signal_fence_and_flush()
+            .unwrap();
+
+        future.wait(None).unwrap();
+
+        let pk = &output_buffer.read().unwrap()[0..32]
+            .iter()
+            .map(|&x| x as u8)
+            .collect::<Vec<_>>();
+
+        let pk64 = BASE64.encode(&pk);
+
+        println!("pk: {pk64} {pk:02x?}");
+
+        let mut nc = [0u8; 32];
+        curve25519_base(&mut nc, sk);
+        let nc64 = BASE64.encode(&nc);
+
+        println!("nc: {nc64} {nc:02x?}");
+
+        let mut wg = std::process::Command::new("wg")
+            .arg("pubkey")
+            .stdin(std::process::Stdio::piped())
+            .stdout(std::process::Stdio::piped())
+            .spawn()
+            .unwrap();
+
+        wg.stdin.take().unwrap().write_all(sk64.as_bytes()).unwrap();
+
+        let wg64 = String::from_utf8(wg.wait_with_output().unwrap().stdout).unwrap();
+        let wg64 = wg64.trim_end();
+        let wg = BASE64.decode(&wg64).unwrap();
+
+        println!("wg: {wg64} {wg:02x?}");
+
+        std::thread::sleep(Duration::from_secs(1));
+    }
 }
---
+++ src/nacl.rs
@@ -0,0 +1,340 @@
+fn add(out: &mut [u32], a: &[u32], b: &[u32]) {
+    let mut u: u32 = 0;
+    for j in 0..31 {
+        u += a[j] + b[j];
+        out[j] = u & 255;
+        u >>= 8;
+    }
+    u += a[31] + b[31];
+    out[31] = u;
+}
+
+fn sub(out: &mut [u32], a: &[u32], b: &[u32]) {
+    let mut u: u32 = 218;
+    for j in 0..31 {
+        u += a[j] + 65280 - b[j];
+        out[j] = u & 255;
+        u >>= 8;
+    }
+    u = u.wrapping_add(a[31].wrapping_sub(b[31]));
+    out[31] = u;
+}
+
+fn squeeze(a: &mut [u32]) {
+    let mut u: u32 = 0;
+    for j in 0..31 {
+        u += a[j];
+        a[j] = u & 255;
+        u >>= 8;
+    }
+    u += a[31];
+    a[31] = u & 127;
+    u = 19 * (u >> 7);
+    for j in 0..31 {
+        u += a[j];
+        a[j] = u & 255;
+        u >>= 8;
+    }
+    u += a[31];
+    a[31] = u;
+}
+
+static MINUSP: [u32; 32] = [
+    19, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    128,
+];
+
+fn freeze(a: &mut [u32]) {
+    let mut aorig: [u32; 32] = [0; 32];
+    for j in 0..32 {
+        aorig[j] = a[j];
+    }
+    add(a, &aorig, &MINUSP);
+    let negative = -(((a[31] >> 7) & 1) as i32) as u32;
+    for j in 0..32 {
+        a[j] ^= negative & (aorig[j] ^ a[j]);
+    }
+}
+
+fn mult(out: &mut [u32], a: &[u32], b: &[u32]) {
+    for i in 0..32 {
+        let mut u: u32 = 0;
+        for j in 0..i + 1 {
+            u += a[j] * b[i - j];
+        }
+        for j in i + 1..32 {
+            u += 38 * a[j] * b[i + 32 - j]
+        }
+        out[i] = u;
+    }
+    squeeze(out);
+}
+
+fn mult121665(out: &mut [u32], a: &[u32]) {
+    let mut u: u32 = 0;
+    for j in 0..31 {
+        u += 121665 * a[j];
+        out[j] = u & 255;
+        u >>= 8;
+    }
+    u += 121665 * a[31];
+    out[31] = u & 127;
+    u = 19 * (u >> 7);
+    for j in 0..31 {
+        u += out[j];
+        out[j] = u & 255;
+        u >>= 8;
+    }
+    u += out[31];
+    out[31] = u;
+}
+
+fn square(out: &mut [u32], a: &[u32]) {
+    for i in 0..32 {
+        let mut u: u32 = 0;
+        let mut j: usize = 0;
+        while j < (i - j) {
+            u += a[j] * a[i - j];
+            j += 1;
+        }
+        j = i + 1;
+        while j < (i + 32 - j) {
+            u += 38 * a[j] * a[i + 32 - j];
+            j += 1;
+        }
+        u *= 2;
+        if (i & 1) == 0 {
+            u += a[i / 2] * a[i / 2];
+            u += 38 * a[i / 2 + 16] * a[i / 2 + 16];
+        }
+        out[i] = u;
+    }
+    squeeze(out);
+}
+
+fn select(p: &mut [u32], q: &mut [u32], r: &[u32], s: &[u32], b: u32) {
+    let bminus1 = b.wrapping_sub(1);
+    for j in 0..64 {
+        let t = bminus1 & (r[j] ^ s[j]);
+        p[j] = s[j] ^ t;
+        q[j] = r[j] ^ t;
+    }
+}
+
+fn mainloop(work1: &mut [u32], work2: &mut [u32], e: &[u32]) {
+    let mut xzm1: [u32; 64] = [0; 64];
+    let mut xzm: [u32; 64] = [0; 64];
+    let mut xzmb: [u32; 64] = [0; 64];
+    let mut xzm1b: [u32; 64] = [0; 64];
+    let mut xznb: [u32; 64] = [0; 64];
+    let mut xzn1b: [u32; 64] = [0; 64];
+    let mut a0: [u32; 64] = [0; 64];
+    let mut a1: [u32; 64] = [0; 64];
+    let mut b0: [u32; 64] = [0; 64];
+    let mut b1: [u32; 64] = [0; 64];
+    let mut c1: [u32; 64] = [0; 64];
+    let mut r: [u32; 32] = [0; 32];
+    let mut s: [u32; 32] = [0; 32];
+    let mut t: [u32; 32] = [0; 32];
+    let mut u: [u32; 32] = [0; 32];
+
+    xzm1[0..32].copy_from_slice(work1);
+    xzm1[32] = 1;
+
+    xzm[0] = 1;
+    for j in 1..64 {
+        xzm[j] = 0;
+    }
+
+    let mut pos: usize = 254;
+    loop {
+        let mut b = e[pos / 8] >> (pos & 7);
+        b &= 1;
+        select(&mut xzmb, &mut xzm1b, &xzm, &xzm1, b);
+        add(&mut a0[0..32], &xzmb[0..32], &xzmb[32..]);
+        sub(&mut a0[32..], &xzmb[0..32], &xzmb[32..]);
+        add(&mut a1[0..32], &xzm1b[0..32], &xzm1b[32..]);
+        sub(&mut a1[32..], &xzm1b[0..32], &xzm1b[32..]);
+        square(&mut b0[0..32], &a0[0..32]);
+        square(&mut b0[32..], &a0[32..]);
+        mult(&mut b1[0..32], &a1[0..32], &a0[32..]);
+        mult(&mut b1[32..], &a1[32..], &a0[0..32]);
+        add(&mut c1[0..32], &b1[0..32], &b1[32..]);
+        sub(&mut c1[32..], &b1[0..32], &b1[32..]);
+        square(&mut r[0..32], &c1[32..]);
+        sub(&mut s[0..32], &b0[0..32], &b0[32..]);
+        mult121665(&mut t[0..32], &s[0..32]);
+        add(&mut u[0..32], &t[0..32], &b0[0..32]);
+        mult(&mut xznb[0..32], &b0[0..32], &b0[32..]);
+        mult(&mut xznb[32..], &s[0..32], &u[0..32]);
+        square(&mut xzn1b[0..32], &c1[0..32]);
+        mult(&mut xzn1b[32..], &r[0..32], &work1);
+        select(&mut xzm, &mut xzm1, &xznb, &xzn1b, b);
+        if pos == 0 {
+            break;
+        }
+        pos -= 1;
+    }
+
+    work1.copy_from_slice(&xzm[0..32]);
+    work2.copy_from_slice(&xzm[32..64]);
+}
+
+fn recip(out: &mut [u32], z: &[u32]) {
+    let mut z2: [u32; 32] = [0; 32];
+    let mut z9: [u32; 32] = [0; 32];
+    let mut z11: [u32; 32] = [0; 32];
+    let mut z2_5_0: [u32; 32] = [0; 32];
+    let mut z2_10_0: [u32; 32] = [0; 32];
+    let mut z2_20_0: [u32; 32] = [0; 32];
+    let mut z2_50_0: [u32; 32] = [0; 32];
+    let mut z2_100_0: [u32; 32] = [0; 32];
+    let mut t0: [u32; 32] = [0; 32];
+    let mut t1: [u32; 32] = [0; 32];
+
+    /* 2 */
+    square(&mut z2, &z);
+    /* 4 */
+    square(&mut t1, &z2);
+    /* 8 */
+    square(&mut t0, &t1);
+    /* 9 */
+    mult(&mut z9, &t0, &z);
+    /* 11 */
+    mult(&mut z11, &z9, &z2);
+    /* 22 */
+    square(&mut t0, &z11);
+    /* 2^5 - 2^0 = 31 */
+    mult(&mut z2_5_0, &t0, &z9);
+
+    /* 2^6 - 2^1 */
+    square(&mut t0, &z2_5_0);
+    /* 2^7 - 2^2 */
+    square(&mut t1, &t0);
+    /* 2^8 - 2^3 */
+    square(&mut t0, &t1);
+    /* 2^9 - 2^4 */
+    square(&mut t1, &t0);
+    /* 2^10 - 2^5 */
+    square(&mut t0, &t1);
+    /* 2^10 - 2^0 */
+    mult(&mut z2_10_0, &t0, &z2_5_0);
+
+    /* 2^11 - 2^1 */
+    square(&mut t0, &z2_10_0);
+    /* 2^12 - 2^2 */
+    square(&mut t1, &t0);
+    /* 2^20 - 2^10 */
+    for _ in 1..5 {
+        square(&mut t0, &t1);
+        square(&mut t1, &t0);
+    }
+    /* 2^20 - 2^0 */
+    mult(&mut z2_20_0, &t1, &z2_10_0);
+
+    /* 2^21 - 2^1 */
+    square(&mut t0, &z2_20_0);
+    /* 2^22 - 2^2 */
+    square(&mut t1, &t0);
+    /* 2^40 - 2^20 */
+    for _ in 1..10 {
+        square(&mut t0, &t1);
+        square(&mut t1, &t0);
+    }
+    /* 2^40 - 2^0 */
+    mult(&mut t0, &t1, &z2_20_0);
+
+    /* 2^41 - 2^1 */
+    square(&mut t1, &t0);
+    /* 2^42 - 2^2 */
+    square(&mut t0, &t1);
+    /* 2^50 - 2^10 */
+    for _ in 1..5 {
+        square(&mut t1, &t0);
+        square(&mut t0, &t1);
+    }
+    /* 2^50 - 2^0 */
+    mult(&mut z2_50_0, &t0, &z2_10_0);
+
+    /* 2^51 - 2^1 */
+    square(&mut t0, &z2_50_0);
+    /* 2^52 - 2^2 */
+    square(&mut t1, &t0);
+    /* 2^100 - 2^50 */
+    for _ in 1..25 {
+        square(&mut t0, &t1);
+        square(&mut t1, &t0);
+    }
+    /* 2^100 - 2^0 */
+    mult(&mut z2_100_0, &t1, &z2_50_0);
+
+    /* 2^101 - 2^1 */
+    square(&mut t1, &z2_100_0);
+    /* 2^102 - 2^2 */
+    square(&mut t0, &t1);
+    /* 2^200 - 2^100 */
+    for _ in 1..50 {
+        square(&mut t1, &t0);
+        square(&mut t0, &t1);
+    }
+    /* 2^200 - 2^0 */
+    mult(&mut t1, &t0, &z2_100_0);
+
+    /* 2^201 - 2^1 */
+    square(&mut t0, &t1);
+    /* 2^202 - 2^2 */
+    square(&mut t1, &t0);
+    /* 2^250 - 2^50 */
+    for _ in 1..25 {
+        square(&mut t0, &t1);
+        square(&mut t1, &t0);
+    }
+    /* 2^250 - 2^0 */
+    mult(&mut t0, &t1, &z2_50_0);
+
+    /* 2^251 - 2^1 */
+    square(&mut t1, &t0);
+    /* 2^252 - 2^2 */
+    square(&mut t0, &t1);
+    /* 2^253 - 2^3 */
+    square(&mut t1, &t0);
+    /* 2^254 - 2^4 */
+    square(&mut t0, &t1);
+    /* 2^255 - 2^5 */
+    square(&mut t1, &t0);
+    /* 2^255 - 21 */
+    mult(out, &t1, &z11);
+}
+
+pub fn curve25519(q: &mut [u8], n: &[u8], p: &[u8]) {
+    let mut work1: [u32; 32] = [0; 32];
+    let mut work2: [u32; 32] = [0; 32];
+    let mut work3: [u32; 32] = [0; 32];
+    let mut e: [u32; 32] = [0; 32];
+    for i in 0..32 {
+        e[i] = n[i] as u32;
+    }
+    e[0] &= 248;
+    e[31] &= 127;
+    e[31] |= 64;
+    for i in 0..32 {
+        work1[i] = p[i] as u32;
+    }
+    mainloop(&mut work1, &mut work2, &e);
+    let mut t_work2: [u32; 32] = [0; 32];
+    t_work2.copy_from_slice(&work2);
+    recip(&mut work2, &t_work2);
+    mult(&mut work3, &work1, &work2);
+    freeze(&mut work3);
+    for i in 0..32 {
+        q[i] = work3[i] as u8;
+    }
+}
+
+const BASE: [u8; 32] = [
+    9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+];
+
+pub fn curve25519_base(q: &mut [u8], n: &[u8]) {
+    return curve25519(q, n, &BASE);
+}
